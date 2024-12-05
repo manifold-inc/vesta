3a8c69af49dd254c2d641060257535cbcb733f75
josh <josh@mjbrowns.com>
2024-12-05 16:57:46 -0600

    feat: time display

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM python:3.10 
 COPY main.py .
-RUN pip install vesta python-dotenv pycoingecko
+RUN pip install vesta python-dotenv pycoingecko pytz
 ENTRYPOINT ["python", "./main.py"] 
--- main.py
+++ main.py
@@ -4,18 +4,38 @@ import os
 import time
 from pycoingecko import CoinGeckoAPI
 from vesta.vbml import Component
+from datetime import datetime
+import pytz
+
+# Get the current time in UTC
+
+# Convert to Chicago timezone
+chicago_tz = pytz.timezone('America/Chicago')
+
+# Format the output
+
 dotenv.load_dotenv()
 
 rw_client = vesta.ReadWriteClient(os.getenv('VEST_KEY', ''))
 vbml_client = vesta.VBMLClient()
 
+tick = 1
+cg = CoinGeckoAPI()
+tao = cg.get_price(ids='bittensor', vs_currencies='usd')['bittensor']['usd']
+eth = cg.get_price(ids='ethereum', vs_currencies='usd')['ethereum']['usd']
+
 while True:
-    cg = CoinGeckoAPI()
-    tao = cg.get_price(ids='bittensor', vs_currencies='usd')['bittensor']['usd']
-    eth = cg.get_price(ids='ethereum', vs_currencies='usd')['ethereum']['usd']
+    if tick % 5 == 0:
+        cg = CoinGeckoAPI()
+        tao = cg.get_price(ids='bittensor', vs_currencies='usd')['bittensor']['usd']
+        eth = cg.get_price(ids='ethereum', vs_currencies='usd')['ethereum']['usd']
+
+    now_utc = datetime.now(pytz.utc)
+    now_chicago = now_utc.astimezone(chicago_tz)
+    now_chicago.strftime("%Y-%m-%d %H:%M:%S %Z")
 
     component = Component(
-            f"{{67}} Tao Price: {tao} {{68}}\n{{67}} Eth Price: {eth} {{68}}",
+            f"{now_chicago}\n{{67}} Tao Price:  {tao} {{68}}\n{{67}} Eth Price: {eth} {{68}}",
         justify="center",
         align="center",
         width=22,
@@ -23,4 +43,4 @@ while True:
     )
 
     rw_client.write_message(vbml_client.compose([component]))
-    time.sleep(5)
+    time.sleep(1)
