f325b6cade2234f3f9b7d1d0b4eed6cdb9be9610
josh <josh@mjbrowns.com>
2024-12-06 10:08:27 -0600

    fix: akt

--- main.py
+++ main.py
@@ -23,19 +23,23 @@ tick = 1
 cg = CoinGeckoAPI()
 tao = cg.get_price(ids="bittensor", vs_currencies="usd")["bittensor"]["usd"]
 eth = cg.get_price(ids="ethereum", vs_currencies="usd")["ethereum"]["usd"]
+akt = cg.get_price(ids="akash-network", vs_currencies="usd")["akash-network"]["usd"]
 while True:
     try:
         if tick % 5 == 0:
             cg = CoinGeckoAPI()
             tao = cg.get_price(ids="bittensor", vs_currencies="usd")["bittensor"]["usd"]
             eth = cg.get_price(ids="ethereum", vs_currencies="usd")["ethereum"]["usd"]
+            akt = cg.get_price(ids="akash-network", vs_currencies="usd")[
+                "akash-network"
+            ]["usd"]
 
         now_utc = datetime.now(pytz.utc).replace(microsecond=0)
         now_chicago = now_utc.astimezone(chicago_tz)
         now_chicago.strftime("%Y-%m-%d\n%H:%M:%S")[:-6]
 
         component = Component(
-            f"{now_chicago}\n{{67}} Tao Price:   {tao} {{68}}\n{{67}} Eth Price: {eth} {{68}}",
+            f"{now_chicago}\n{{67}} Tao Price:  {tao:.2f} {{68}}\n{{67}} Eth Price: {eth:.2f} {{68}}\n{{67}} AKT Price:    {akt:.2f} {{68}}",
             justify="center",
             align="center",
             width=22,
